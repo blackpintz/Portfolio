cf2465a7a7b0f4860711b90ea003a2f38d0bebf9
Rose <rwwanjohi10@gmail.com>
2019-08-14 10:58:25 +0300

    Updates

---
+++ precache-manifest.0cde1928208e6b511cdd317dae782e3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ec7daf14f454474d177ec7abcf365a",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "c471c6d10791528c36bd",
+    "url": "/Portfolio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "1b4d327e10de82eb92f7",
+    "url": "/Portfolio/static/js/2.a31939dd.chunk.js"
+  },
+  {
+    "revision": "c471c6d10791528c36bd",
+    "url": "/Portfolio/static/js/main.ad1919bc.chunk.js"
+  },
+  {
+    "revision": "02bb7fa0c0195f9aa52b",
+    "url": "/Portfolio/static/js/runtime~main.aeacf694.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
